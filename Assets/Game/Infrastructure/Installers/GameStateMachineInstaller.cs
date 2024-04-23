0112422b54bcf971e1dc3d105284476a08407380
inrage <inrag3x@gmail.com>
2024-04-23 23:50:43 +0300

    add: gamestatemachine installer

---
+++ Assets/Game/Infrastructure/Installers/GameStateMachineInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Game.Infrastructure.States;
+using Zenject;
+
+namespace Game.Infrastructure.Installers
+{
+    public class GameStateMachineInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<GameStateMachine>().AsSingle().NonLazy();
+
+            Container.Bind<BootstrapState>().AsSingle().NonLazy();
+        }
+    }
+}
